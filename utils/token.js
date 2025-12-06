adb6d8557723a0e57b2f772dfbd864d407b95e25
erAbhishekJaiswal <erabhishek72@gmail.com>
2025-12-06 12:50:40 +0530

    update the token time

--- utils/token.js
+++ utils/token.js
@@ -2,10 +2,10 @@ const jwt = require("jsonwebtoken");
 
 // Use environment variables in production
 const JWT_SECRET = process.env.JWT_SECRET || "replace_this_with_a_long_secret";
-const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "15m";
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "1d";
 const JWT_REFRESH_SECRET =
   process.env.JWT_REFRESH_SECRET || "replace_refresh_secret";
-const JWT_REFRESH_EXPIRES_IN = process.env.JWT_REFRESH_EXPIRES_IN || "7d";
+const JWT_REFRESH_EXPIRES_IN = process.env.JWT_REFRESH_EXPIRES_IN || "1d";
 
 function signAccessToken(payload) {
   return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
